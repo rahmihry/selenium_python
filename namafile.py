08248d433693f35e1fd9dba3d43ad80202def9ad
rahmihry <137986165+rahmihry@users.noreply.github.com>
2023-06-30 17:51:39 +0700

    Update namafile.py

--- namafile.py
+++ namafile.py
@@ -42,6 +42,20 @@ class TestLogin(unittest.TestCase):
         response_data = driver.find_element(By.CSS_SELECTOR,"[data-test='error']").text
         self.assertIn('Epic sadface: Username and password do not match any user in this service', response_data)
 
+    def test_a_failed_login_empty_username(self):
+        # steps
+        driver = self.browser #buka web browser
+        driver.get(self.url) # buka situs
+        #time.sleep(3)
+        driver.find_element(By.ID,"user-name").send_keys("") # isi email
+        #time.sleep(1)
+        driver.find_element(By.ID,"password").send_keys("secret_sauce") # isi password
+        #time.sleep(1)
+        driver.find_element(By.ID, "login-button").click()
+        # validasi
+        response_data = driver.find_element(By.CSS_SELECTOR,"[data-test='error']").text
+        self.assertIn('Epic sadface: Username is required', response_data)
+
     def tearDown(self):
         self.browser.close()
 
